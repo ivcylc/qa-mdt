38456c8f187b774073d613f18a4fc28b271bb7e3
ivcylc <118931037+ivcylc@users.noreply.github.com>
2024-11-21 17:59:11 +0800

    Update infer.sh

--- infer/infer.sh
+++ infer/infer.sh
@@ -2,7 +2,14 @@
 
 # TODO: change the yaml, text list, ckpt path
 
+# for better music quality
 python3 infer_mos5.py \
 --config_yaml audioldm_train/config/mos_as_token/qa_mdt.yaml \
 --list_inference prompts/good_prompts_1.lst \
 --reload_from_ckpt "output/model_checkpoint.ckpt"
+
+# for better overrall quality
+# python3 infer_mos4.py \
+# --config_yaml audioldm_train/config/mos_as_token/qa_mdt.yaml \
+# --list_inference prompts/good_prompts_1.lst \
+# --reload_from_ckpt "output/model_checkpoint.ckpt"
